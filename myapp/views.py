4c3ea8cbe51bc0858f6873c3a8280324ad501446
agentepeke <agentepeke@gmail.com>
2024-02-26 21:50:13 -0600

    Ejercicio con gjango

---
+++ myapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404, render
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+def hello(request, username):
+    print(username)
+    return HttpResponse("<h2>Hello %s</h2>" % username)
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return render(request, 'projects.html')
+
+def tasks(request):
+    #task =Task.objects.get(title=title)
+    return render(request, 'tasks.html')
